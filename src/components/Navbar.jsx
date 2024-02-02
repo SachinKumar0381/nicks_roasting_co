3fdfe1a4fc2215ad59dbe6a76ad5428cd93adffc
SachinKumar0381 <sachinkuom0591998@gmail.com>
2024-02-02 12:53:41 +0530

    scrolling navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import  "../assets/css/bootstrap.min.css"
 import "../assets/css/fontawesome.css";
 import "../assets/css/animate.css";
@@ -23,9 +23,37 @@ const Navbar = ({handleSidebar}) => {
             setToggleMenu("menu_anim main_menu_inner navbar-collapse menu_active")
         }
     }
+
+
+    // Code for changing backgroundColor of Navbar on scrolling
+
+    const [backgroundColor, setBackgroundColor] = useState('none');
+      
+        useEffect(() => {
+          const handleScroll = () => {
+            // Calculate the scroll percentage
+            const scrollPercentage = (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100;
+      
+            // Change the background color based on the scroll percentage
+            // You can adjust the conditions and colors according to your preference
+            if (scrollPercentage < 2) {
+              setBackgroundColor('none');
+            } else {
+              setBackgroundColor('white');
+            }
+          };
+      
+          // Add a scroll event listener to the window
+          window.addEventListener('scroll', handleScroll);
+      
+          // Remove the event listener when the component unmounts
+          return () => {
+            window.removeEventListener('scroll', handleScroll);
+          };
+        }, [])
   return (
     <div>
-        <header className="header_section style_2" style={{position:"fixed", zIndex:"9999"}}>
+        <header className="header_section style_2" style={{position:"fixed", zIndex:"9999", backgroundColor:`${backgroundColor==="none"?"rgba(0, 0, 0, 0.4)":"rgb(241 198 154)"}`}}>
         <div className="content_wrap">
           <div className="container maxw_1560">
             <div className="row align-items-center">
@@ -41,10 +69,10 @@ const Navbar = ({handleSidebar}) => {
               <div className="col-lg-10 col-md-6 col-6">
                 <nav className="main_menu navbar navbar-expand-lg">
                   <button className="mobile_menu_btn navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_menu_dropdown" aria-controls="main_menu_dropdown" aria-expanded="false" aria-label="Toggle navigation" onClick={handleToggleMenu}>
-                    <span className="navbar-toggler-icon"><i className="fal fa-bars"></i></span>
+                    <span className="navbar-toggler-icon"><i className="fal fa-bars" style={{color:`${backgroundColor==="none"?"white":"black"}`}}></i></span>
                   </button>
                   <div className={toggleMenu} id="main_menu_dropdown">
-                    <ul className="main_menu_list ul_li">
+                    <ul className={backgroundColor==="none"?'main_menu_list ul_li main_menu_list_color':'main_menu_list ul_li main_menu_list_color_2'}>
                       <li>
                         <a className="nav-link" href="#">
                           Home
@@ -91,18 +119,18 @@ const Navbar = ({handleSidebar}) => {
 
                   <ul className="header_btns_group ul_li_right">
                     <li>
-                      <button type="button" className="main_search_btn" data-bs-toggle="collapse" data-bs-target="#main_search_collapse" aria-expanded="false" aria-controls="main_search_collapse">
+                      <button type="button" className="main_search_btn clr_black" data-bs-toggle="collapse" data-bs-target="#main_search_collapse" aria-expanded="false" aria-controls="main_search_collapse" style={{color:`${backgroundColor==="none"?"white":"black"}`}}>
                         <i className="fal fa-search"></i>
                       </button>
                     </li>
                     <li>
-                      <button type="button" className="cart_btn" onClick={()=>handleSidebar()}>
+                      <button type="button" className="cart_btn" onClick={()=>handleSidebar()} style={{color:`${backgroundColor==="none"?"white":"black"}`}}>
                         <i className="fal fa-shopping-bag"></i>
                         <small className="cart_counter">2</small>
                       </button>
                     </li>
                     <li>
-                      <a className="btn btn_primary text-uppercase" href="#">Login</a>
+                      <a className="btn btn_primary text-uppercase" href="#" style={{color:`${backgroundColor==="none"?"white":"black"}`}}>Login</a>
                     </li>
                   </ul>
                 </nav>
