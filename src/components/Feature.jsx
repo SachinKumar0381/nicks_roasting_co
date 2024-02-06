b4fd9c4aab254232d3cce4dd032e87d52b9f3655
SachinKumar0381 <sachinkuom0591998@gmail.com>
2024-02-06 12:21:45 +0530

    changes in features section

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -19,17 +19,20 @@ import scratch_01 from "../assets/images/decorations/scratch_01.png";
 const Feature = () => {
   return (
     <section className="feature_primary_section2 deco_wrap">
+            <div className="section_title text-uppercase text-center">
+              <h2 className="small_title wow fadeInUp" data-wow-delay=".2s"><i className="fas fa-coffee"></i> HOW IT WORKS</h2>
+            </div>
           <div className="container">
             <div className="row">
               <div className="col-lg-3 col-md-4 col-sm-6">
                 <div className="feature_primary wow fadeInUp" data-wow-delay=".1s">
                   <div className="item_icon">
                     <span className="item_serial">1</span>
-                    <img src={icon_01} alt="icon_not_found"/>
+                    STEP
                   </div>
-                  <h3 className="item_title text-uppercase">Text will be coming soon...</h3>
+                  <h3 className="item_title text-uppercase">CHOOSE YOUR COFFEE</h3>
                   <p className="mb-0">
-                    Text will be coming soon...
+                  Get started with our Starter Kit, which includes a selection of our most popular coffee blends. You can choose from whole bean or ground coffee, and we offer a range of roasts to suit your taste.
                   </p>
                 </div>
               </div>
@@ -38,11 +41,11 @@ const Feature = () => {
                 <div className="feature_primary wow fadeInUp" data-wow-delay=".2s">
                   <div className="item_icon">
                     <span className="item_serial">2</span>
-                    <img src={icon_02} alt="icon_not_found"/>
+                    STEP
                   </div>
-                  <h3 className="item_title text-uppercase">Text will be coming soon...</h3>
+                  <h3 className="item_title text-uppercase">SELECT YOUR GRIND TYPE</h3>
                   <p className="mb-0">
-                    Text will be coming soon...
+                  Let our expert roasters select the perfect coffee for you with our Roaster's Picks subscription. Each month, we will send you a different coffee selected by our roasters, so you can try a variety of blends and roasts.
                   </p>
                 </div>
               </div>
@@ -51,11 +54,11 @@ const Feature = () => {
                 <div className="feature_primary wow fadeInUp" data-wow-delay=".3s">
                   <div className="item_icon">
                     <span className="item_serial">3</span>
-                    <img src={icon_03} alt="icon_not_found"/>
+                    STEP
                   </div>
-                  <h3 className="item_title text-uppercase">Text will be coming soon...</h3>
+                  <h3 className="item_title text-uppercase">SELECT YOUR DELIVERY FREQUENCY</h3>
                   <p className="mb-0">
-                    Text will be coming soon...
+                  Join our Coffee of the Month club and receive a different coffee each month, carefully selected by our roasters. With this subscription, you'll be able to try a range of coffee flavors and roasts, and discover new favorites.
                   </p>
                 </div>
               </div>
@@ -64,19 +67,15 @@ const Feature = () => {
                 <div className="feature_primary wow fadeInUp" data-wow-delay=".4s">
                   <div className="item_icon">
                     <span className="item_serial">4</span>
-                    <img src={icon_04} alt="icon_not_found"/>
+                    STEP
                   </div>
-                  <h3 className="item_title text-uppercase">Text will be coming soon...</h3>
+                  <h3 className="item_title text-uppercase">ENJOY FRESH COFFEE DELIVERED TO YOUR DOOR</h3>
                   <p className="mb-0">
-                    Text will be coming soon...
+                  We roast our coffee in small batches, ensuring maximum freshness and flavor in every bag you receive.
                   </p>
                 </div>
               </div>
             </div>
-          </div>
-
-          <div className="scratch scratch_bottom">
-            <img src={scratch_01} alt="image_not_found"/>
           </div>
         </section>
   )
